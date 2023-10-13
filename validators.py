059f53c798450897f877c0128a199ef328bfe390
DryBasic <tb@Tariks-MacBook-Pro.local>
2023-10-13 13:34:59 -0700

    progress

--- validators.py
+++ validators.py
@@ -1,5 +1,5 @@
-import yaml 
-
+import yaml
+import gs_shape as shape
 
 class GlobalSchema:
     """Object to load global_schema.yml
@@ -8,48 +8,36 @@ class GlobalSchema:
     * Holds methods for validating graph mappings against global schema
     """
 
-    def __init__(self, path='./global_schema.yml', print_val_results=True) -> None:
-        self.validation_results = []
-        self.print_val_results = print_val_results
+    def __init__(self, path='./global_schema.yml') -> None:
         with open(path) as file: 
             self.schema = yaml.safe_load(file)
 
-        self.validate_schema()        
+        self.validate_schema()
 
-    def validate_schema(self) -> None:
-        self.validation_state(
-            lambda x: x == ('node_types', 'edge_types'),
-            'Appropriate keys')(tuple(self.schema.keys()))
-        
-        names = []
-        edges = []
-        for node_type in self.schema.get('node_types'):
-            self.validation_state(
-                lambda x: x == ('name', 'id_dtype', 'properties', 'edges'),
-                f'"{node_type.get("name")}" Appropriate keys')(tuple(node_type.keys()))
+    def validate_schema(self):
+        # validate schema shape using "schema" package
+        shape.global_schema.validate(self.schema)
 
-            if (name := node_type.get('name')) not in names:
-                names.append(node_type)
-            else:
-                self.validation_state(lambda: False
-                    , f'All unique node_type names {name}')()
-                break
-
-        
-        if not all(self.validation_results):
-            raise Exception("One or more test cases failed. ")
+        # validate referential properties in accordance with neo4j rules
+        # 1: edges should be referenced by both partners 
+        # 2: node types should be unique
+        # 3: edge types should be unique
+        for node in self.schema.get('node_types'):
+            name = node.get('name')
+            for edge in node.get('edges'):
+                pass
 
     def validate_mapping(self, map_path, map_type) -> bool:
         pass
 
-    def validation_state(self, func, msg) -> function:
-        def wrapper(*args, **kwargs):
-            state_bool = func(*args, **kwargs)
-            state = 'Pass' if state_bool else 'Fail'
-            self.validation_results.append(state_bool)
-            if self.print_val_results:
-                print(f'{msg}: {state}')
-        return wrapper
+    # def validation_state(self, func, msg):
+    #     def wrapper(*args, **kwargs):
+    #         state_bool = func(*args, **kwargs)
+    #         state = 'Pass' if state_bool else 'Fail'
+    #         self.validation_results.append(state_bool)
+    #         if self.print_val_results:
+    #             print(f'{msg}: {state}')
+    #     return wrapper
 
 
     
